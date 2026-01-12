c4704f8070bffa66b49bc737696a230b18627fe6
Sudeep <sudeeppprajapati@gmail.com>
2026-01-12 18:46:33 +0530

    config: update TypeScript and Next.js configurations
    - Set TypeScript target to ES2022 in tsconfig.json for modern features
    - Ensure latest moduleResolution settings

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
